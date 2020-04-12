77e078a124a2c51786f934a3fee51e92f0d9054f
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-11 23:43:50 -0400

    added FILEPATH variable

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import argparse
+import os
+from src.getAllFiles import getAllFiles
+from src.getAllFileFormats import getAllFileFormats
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument("main_branch", type="str", help="the starting branch. any files in this branch will be organized")
+argParser.add_argument("main_branch", type=str, help="the starting branch. any files in this branch will be organized")
 
 args = argParser.parse_args()
+
+FILEPATH = args.main_branch.replace("/", "//")
