080a2bac5db016a551e2eba1adc55c4a9bdacf1f
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-12 15:21:30 -0400

    fixed to fit with the class

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import argparse
-import os
-from src.getAllFiles import getAllFiles
-from src.getAllFileFormats import getAllFileFormats
+from src.OrganizeByFileFormat import OrganizeByFileFormat
 
+'Arguments'
 argParser = argparse.ArgumentParser()
 argParser.add_argument("main_branch", type=str, help="the starting branch. any files in this branch will be organized")
 
 args = argParser.parse_args()
 
-FILEPATH = args.main_branch.replace("/", "//")
+'FileFormatOrganization'
+def main():
+    global args
+    BASEFILEPATH = args.main_branch.replace("/", "//")
+
+    object = OrganizeByFileFormat(BASEFILEPATH)
+    object.getAllFiles()
+    object.print_test()
+    object.getAllFileFormats()
+    object.print_test()
+    object.createFiles()
+    object.print_test()
+    object.moveFiles(BASEFILEPATH)
+    object.print_test()
+
+main()
