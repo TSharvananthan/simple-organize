39e8e7b7ad8db7ba0f7d0a380fc1556f2667330a
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-17 13:43:03 -0400

    removed moveFiles

--- src/template files/moveFiles.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def moveFiles(files: list, formats: list, basefile: str, filenames: list) -> None:
-    for file, format, name in zip(files, formats, filenames):
-        os.replace(file, f"{basefile}\\{format}\\{name}")
