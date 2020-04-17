8a30a0c7aadf09c8d79b9e419fa09b6040fa7d6d
Thanussian Sharvananthan <thanussian@gmail.com>
2020-04-17 13:43:02 -0400

    removed createFiles

--- src/template files/createFiles.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def createFiles(listofformats: list) -> None:
-    for format in listofformats:
-        try:
-            os.mkdir(format)
-        except FileExistsError:
-            pass
